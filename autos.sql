304d8f8c1eee8bf93fce9940b2a2ef9944719130
Juan-M-L <juanmartinycoco@gmail.com>
2024-10-01 14:50:38 -0300

    Eliminada la tabla marca. Añadida la tabla modelo

--- autos.sql
+++ autos.sql
@@ -1,45 +1,56 @@
--- Creacion de tabla Marca.
-
-CREATE TABLE Marca (
-	MarcaId INT PRIMARY KEY AUTO_INCREMENT,
-    MarcaNombre VARCHAR(20) NOT NULL UNIQUE
+-- Tabla Modelo.
+CREATE TABLE Modelo (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(100) NOT NULL UNIQUE,
+    Anio INT NOT NULL,
+    Capacidad INT NOT NULL,
+    Combustible VARCHAR(50) NOT NULL
 );
 
--- Creacion de tabla Auto.
-
+-- Tabla Auto.
 CREATE TABLE Auto (
-    AutoId INT PRIMARY KEY AUTO_INCREMENT,
-    MarcaId INT NOT NULL,
-    AutoModelo VARCHAR(50) NOT NULL,
-    AutoCombustible VARCHAR(50) NOT NULL,
-    AutoKilometraje INT NOT NULL,
-    AutoPrecio NUMERIC(10,2) NOT NULL
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Marca VARCHAR(50) NOT NULL,
+    ModeloId INT NOT NULL,
+    Kilometraje INT NOT NULL,
+    Precio NUMERIC(10,2) NOT NULL
 );
 
 ALTER TABLE Auto
-ADD FOREIGN KEY (MarcaId)
-REFERENCES Marca(MarcaId);
+ADD FOREIGN KEY (ModeloId)
+REFERENCES Modelo(Id);
 
--- Operaciones de insercion.
+-- Inserción de datos en Modelo
+INSERT INTO Modelo (Nombre, Anio, Capacidad, Combustible) VALUES
+('Ferrari 488 GTB', 2020, 2, 'Gasolina'),
+('Ferrari Portofino', 2021, 4, 'Gasolina'),
+('Lamborghini Huracan', 2020, 2, 'Gasolina'),
+('Lamborghini Aventador', 2021, 2, 'Gasolina'),
+('Porsche Panamera', 2022, 4, 'Híbrido'),
+('Porsche Taycan', 2023, 4, 'Eléctrico'),
+('Bentley Continental GT', 2020, 4, 'Gasolina'),
+('Bentley Flying Spur', 2022, 4, 'Gasolina'),
 
-INSERT INTO Marca(MarcaNombre)
-VALUES 
-    ('Mercedez-Benz'),
-    ('BMW'),
-    ('Audi'),
-    ('Lexus');
 
-INSERT INTO Auto (MarcaId, AutoModelo, AutoCombustible, AutoKilometraje, AutoPrecio)
-VALUES 
-    (1, 'A-Class', 'Nafta', 10000, 999999.99),
-    (1, 'C-Class', 'Nafta', 15000, 1200000.00),
-    (1, 'E-Class', 'Nafta', 20000, 1500000.00),
-    (2, '3 Series', 'Nafta', 5000, 800000.50),
-    (2, '5 Series', 'Nafta', 10000, 1000000.00),
-    (2, 'X5', 'Nafta', 8000, 1300000.00),
-    (3, 'A3', 'Nafta', 12000, 700000.00),
-    (3, 'Q5', 'Nafta', 15000, 900000.00),
-    (3, 'e-tron', 'Eléctrico', 10000, 999999.99),
-    (4, 'ES 350', 'Nafta', 10000, 850000.00),
-    (4, 'RX 500h', 'Híbrido', 8000, 1100000.00),
-    (4, 'NX 450h+', 'Híbrido', 5000, 1200000.00);
+-- Inserción de datos en Auto
+INSERT INTO Auto (Marca, ModeloId, Kilometraje, Precio) VALUES
+('Ferrari', 1, 5000, 250000.00),
+('Ferrari', 2, 3000, 220000.00),
+('Ferrari', 1, 1000, 230000.00),
+('Ferrari', 2, 200, 500000.00),
+('Ferrari', 1, 1500, 270000.00),
+('Lamborghini', 3, 4000, 200000.00),
+('Lamborghini', 4, 2500, 400000.00),
+('Lamborghini', 3, 10000, 220000.00),
+('Lamborghini', 4, 500, 350000.00),
+('Lamborghini', 3, 8000, 180000.00),
+('Porsche', 5, 6000, 150000.00),
+('Porsche', 6, 12000, 80000.00),
+('Porsche', 5, 3000, 120000.00),
+('Porsche', 6, 500, 130000.00),
+('Porsche', 5, 7000, 90000.00),
+('Bentley', 7, 4000, 220000.00),
+('Bentley', 8, 8000, 180000.00),
+('Bentley', 7, 3000, 250000.00),
+('Bentley', 8, 10000, 300000.00),
+('Bentley', 7, 200, 400000.00);
