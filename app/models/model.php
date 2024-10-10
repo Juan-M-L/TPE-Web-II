692130fc245b656105679b486849c8ba960e6b3e
NikosLettos <145607327+NikosLettos@users.noreply.github.com>
2024-10-09 23:08:27 -0300

    Update model.php

--- app/models/model.php
+++ app/models/model.php
@@ -14,4 +14,20 @@ class Model {
         $data->execute();
         return $data->fetchAll();
     }
+
+    // Obtiene todas las categorías (marcas)
+    public function getAllCategories() {
+        $data = $this->db->prepare("SELECT * FROM modelo;");
+        $data->execute();
+        return $data->fetchAll();
+    }
+
+    // Obtiene vehículos por categoría
+    public function getVehiclesByCategory($categoryId) {
+        $data = $this->db->prepare("SELECT * FROM auto JOIN modelo ON auto.ModeloId = modelo.Id WHERE modelo.Id = :categoryId;");
+        $data->bindParam(':categoryId', $categoryId);
+        $data->execute();
+        return $data->fetchAll();
+    }
+    
 }
